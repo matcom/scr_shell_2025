24d2b2c2c274afa7b8abdc69282da962afb59075
ALbertEO3 <marichalalberto292@gmail.com>
2025-03-24 22:41:29 -0400

    run

--- shell.py
+++ shell.py
@@ -1,3 +1,12 @@
 class Shell:
     def __init__(self):
         pass
+
+    def run(self):
+        while True:
+            print("$", end="", flush=True)
+
+
+if __name__ == "__main__":
+    shell = Shell()
+    shell.run()
